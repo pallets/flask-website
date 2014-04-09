a9251a5af02ddc8a2abaa40083068928b95fec0a
Armin Ronacher <armin.ronacher@active-4.com>
2014-04-09 23:56:18 +0200

    Removed old config keys

--- websiteconfig.py
+++ websiteconfig.py
@@ -9,13 +9,6 @@ DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'flask-website.db')
 DATABASE_CONNECT_OPTIONS = {}
 ADMINS = frozenset(['http://lucumr.pocoo.org/'])
 
-THREADS_PER_PAGE = 15
-MAILINGLIST_PATH = os.path.join(_basedir, '_mailinglist')
-INCOMING_MAIL_FOLDER = '_mailinglist/incoming'
-THREAD_FOLDER = '_mailinglist/threads'
-LIST_NAME = 'flask'
-RSYNC_PATH = 'librelist.com::json/%s'
-SUBJECT_PREFIX = '[flask]'
 WHOOSH_INDEX = os.path.join(_basedir, 'flask-website.whoosh')
 DOCUMENTATION_PATH = os.path.join(_basedir, '../flask/docs/_build/dirhtml')
 
